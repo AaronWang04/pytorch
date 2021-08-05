AaronWang04/pytorch

commit 8963ac6105eba7fefc39749902ece5f2ae06e4dc

Report more information for memory profiling (#61282)
Summary:
Report pointed memory size, total allocated memory, total reserved size all in one report.

`ptr` and `alloc_size` will be used for associating with op trace.
`allocated_size`, `reserved_size` will be used for memory trace.

Pull Request resolved: https://github.com/pytorch/pytorch/pull/61282

Reviewed By: ejguan

Differential Revision: D29796282

Pulled By: chaekit

fbshipit-source-id: 5314c867632d3af1fa9a3811b35eaa5e931a5d87

torch/csrc/autograd/profiler_kineto.cpp
@@ -123,8 +123,10 @@ struct KinetoThreadLocalState : public ProfilerThreadLocalState {
 
   // TODO: use kineto
   void reportMemoryUsage(
-      void* /* unused */,
+      void* ptr,
       int64_t alloc_size,
+      int64_t total_allocated,
+      int64_t total_reserved,
       c10::Device device) override {
     if (config_.profile_memory && config_.state != ProfilerState::Disabled) {
       std::lock_guard<std::mutex> guard(state_mutex_);
@@ -137,24 +139,32 @@ struct KinetoThreadLocalState : public ProfilerThreadLocalState {
             cpu_trace->span,
             libkineto::ActivityType::CPU_INSTANT_EVENT,
             kMemoryEventName));
-      auto& act = cpu_trace->activities.back();
-      act.device = libkineto::processId();
-      act.resource = libkineto::systemThreadId();
+        auto& act = cpu_trace->activities.back();
+        act.device = libkineto::processId();
+        act.resource = libkineto::systemThreadId();
 
-      act.startTime = start_time;
-      act.addMetadata("Device Type", std::to_string((int8_t)device.type()));
-      act.addMetadata("Device Id", std::to_string(device.index()));
-      act.addMetadata("Bytes", std::to_string(alloc_size));
+        act.startTime = start_time;
+        act.addMetadata("Device Type", std::to_string((int8_t)device.type()));
+        act.addMetadata("Device Id", std::to_string(device.index()));
+        act.addMetadata(
+            "Addr", std::to_string(reinterpret_cast<intptr_t>(ptr)));
+        act.addMetadata("Bytes", std::to_string(alloc_size));
+        if (total_allocated >= 0) {
+          act.addMetadata("Allocated Bytes", std::to_string(total_allocated));
+        }
+        if (total_reserved >= 0) {
+          act.addMetadata("Reserved Bytes", std::to_string(total_reserved));
+        }
 #endif // USE_KINETO
-
-      kineto_events_.emplace_back();
-      auto& evt = kineto_events_.back();
-      evt.name(kMemoryEventName)
-        .startUs(start_time)
-        .deviceIndex(device.index())
-        .deviceType(device.type())
-        .nBytes(alloc_size)
-        .startThreadId(at::RecordFunction::currentThreadId());
+	
+	kineto_events_.emplace_back();
+	auto& evt = kineto_events_.back();
+	      evt.name(kMemoryEventName)
+		.startUs(start_time)
+		.deviceIndex(device.index())
+		.deviceType(device.type())
+		.nBytes(alloc_size)
+		.startThreadId(at::RecordFunction::currentThreadId());
     }
   }
 

aten/src/ATen/test/cuda_reportMemoryUsage_test.cpp
@@ -1,0 +1,69 @@
+#include "reportMemoryUsage.h"
+
+#include <gtest/gtest.h>
+
+#include <c10/cuda/CUDACachingAllocator.h>
+
+TEST(DeviceCachingAllocator, check_reporter) {
+  auto reporter = std::make_shared<TestMemoryReportingInfo>();
+  c10::DebugInfoGuard guard(c10::DebugInfoKind::PROFILER_STATE, reporter);
+
+  auto _200kb = 200 * 1024;
+  auto _500mb = 500 * 1024 * 1024;
+
+  auto allocator = c10::cuda::CUDACachingAllocator::get();
+
+  auto alloc1 = allocator->allocate(_200kb);
+  auto r = reporter->getLatestRecord();
+  EXPECT_EQ(alloc1.get(), r.ptr);
+  EXPECT_LE(_200kb, r.alloc_size);
+  EXPECT_LE(_200kb, r.total_allocated);
+  EXPECT_LE(_200kb, r.total_reserved);
+  EXPECT_TRUE(r.device.is_cuda());
+
+  auto alloc1_true_ptr = r.ptr;
+  auto alloc1_true_alloc_size = r.alloc_size;
+
+  // I bet pytorch will not waste that much memory
+  EXPECT_LT(r.total_allocated, 2 * _200kb);
+  // I bet pytorch will not reserve that much memory
+  EXPECT_LT(r.total_reserved, _500mb);
+
+  auto alloc2 = allocator->allocate(_500mb);
+  r = reporter->getLatestRecord();
+  EXPECT_EQ(alloc2.get(), r.ptr);
+  EXPECT_LE(_500mb, r.alloc_size);
+  EXPECT_LE(_200kb + _500mb, r.total_allocated);
+  EXPECT_LE(_200kb + _500mb, r.total_reserved);
+  EXPECT_TRUE(r.device.is_cuda());
+  auto alloc2_true_ptr = r.ptr;
+  auto alloc2_true_alloc_size = r.alloc_size;
+
+  auto max_reserved = r.total_reserved;
+
+  alloc1.clear();
+  r = reporter->getLatestRecord();
+  EXPECT_EQ(alloc1_true_ptr, r.ptr);
+  EXPECT_EQ(-alloc1_true_alloc_size, r.alloc_size);
+  EXPECT_EQ(alloc2_true_alloc_size, r.total_allocated);
+  // alloc2 remain, it is a memory free operation, so it shouldn't reserve more
+  // memory.
+  EXPECT_TRUE(
+      alloc2_true_alloc_size <= r.total_reserved &&
+      r.total_reserved <= max_reserved);
+  EXPECT_TRUE(r.device.is_cuda());
+
+  alloc2.clear();
+  r = reporter->getLatestRecord();
+  EXPECT_EQ(alloc2_true_ptr, r.ptr);
+  EXPECT_EQ(-alloc2_true_alloc_size, r.alloc_size);
+  EXPECT_EQ(0, r.total_allocated);
+  EXPECT_TRUE(0 <= r.total_reserved && r.total_reserved <= max_reserved);
+  EXPECT_TRUE(r.device.is_cuda());
+}
+
+int main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  c10::cuda::CUDACachingAllocator::init(1);
+  return RUN_ALL_TESTS();
+}

aten/src/ATen/test/reportMemoryUsage_test.cpp
@@ -1,0 +1,46 @@
+#include "reportMemoryUsage.h"
+
+#include <gtest/gtest.h>
+
+#include <c10/core/CPUAllocator.h>
+
+TEST(DefaultCPUAllocator, check_reporter) {
+  auto reporter = std::make_shared<TestMemoryReportingInfo>();
+  c10::DebugInfoGuard guard(c10::DebugInfoKind::PROFILER_STATE, reporter);
+
+  auto allocator = c10::GetCPUAllocator();
+
+  auto alloc1 = allocator->allocate(42);
+  auto r = reporter->getLatestRecord();
+  EXPECT_EQ(alloc1.get(), r.ptr);
+  EXPECT_EQ(42, r.alloc_size);
+  EXPECT_EQ(42, r.total_allocated);
+  EXPECT_EQ(0, r.total_reserved);
+  EXPECT_TRUE(r.device.is_cpu());
+
+  auto alloc2 = allocator->allocate(1038);
+  r = reporter->getLatestRecord();
+  EXPECT_EQ(alloc2.get(), r.ptr);
+  EXPECT_EQ(1038, r.alloc_size);
+  EXPECT_EQ(1080, r.total_allocated);
+  EXPECT_EQ(0, r.total_reserved);
+  EXPECT_TRUE(r.device.is_cpu());
+
+  auto alloc1_ptr = alloc1.get();
+  alloc1.clear();
+  r = reporter->getLatestRecord();
+  EXPECT_EQ(alloc1_ptr, r.ptr);
+  EXPECT_EQ(-42, r.alloc_size);
+  EXPECT_EQ(1038, r.total_allocated);
+  EXPECT_EQ(0, r.total_reserved);
+  EXPECT_TRUE(r.device.is_cpu());
+
+  auto alloc2_ptr = alloc2.get();
+  alloc2.clear();
+  r = reporter->getLatestRecord();
+  EXPECT_EQ(alloc2_ptr, r.ptr);
+  EXPECT_EQ(-1038, r.alloc_size);
+  EXPECT_EQ(0, r.total_allocated);
+  EXPECT_EQ(0, r.total_reserved);
+  EXPECT_TRUE(r.device.is_cpu());
+}

aten/src/ATen/MapAllocator.cpp
@@ -352,7 +352,7 @@ MapAllocator::MapAllocator(WithFd, std::string filename, int fd, int flags, size
     }
   }
 #endif
-  c10::reportMemoryUsageToProfiler(base_ptr_, size_, c10::Device(c10::DeviceType::CPU));
+  c10::reportMemoryUsageToProfiler(base_ptr_, size_, 0, size_, c10::Device(c10::DeviceType::CPU));
 }
 
 MapAllocator::MapAllocator(std::string filename, int flags, size_t size)
@@ -600,7 +600,7 @@ void* RefcountedMapAllocator::data() const {
 MapAllocator::~MapAllocator() {
   // NOLINTNEXTLINE(clang-analyzer-optin.cplusplus.VirtualCall)
   close();
-  c10::reportMemoryUsageToProfiler(base_ptr_, -size_, c10::Device(c10::DeviceType::CPU));
+  c10::reportMemoryUsageToProfiler(base_ptr_, -size_, 0, 0, c10::Device(c10::DeviceType::CPU));
 }
 
 }  // namespace at

aten/src/ATen/test/CMakeLists.txt
@@ -36,6 +36,7 @@ list(APPEND ATen_CPU_TEST_SRCS
   ${CMAKE_CURRENT_SOURCE_DIR}/pow_test.cpp
   ${CMAKE_CURRENT_SOURCE_DIR}/variant_test.cpp
   ${CMAKE_CURRENT_SOURCE_DIR}/reduce_ops_test.cpp
+  ${CMAKE_CURRENT_SOURCE_DIR}/reportMemoryUsage_test.cpp
   ${CMAKE_CURRENT_SOURCE_DIR}/memory_format_test.cpp
   ${CMAKE_CURRENT_SOURCE_DIR}/cpu_rng_test.cpp
   ${CMAKE_CURRENT_SOURCE_DIR}/ivalue_test.cpp
@@ -53,6 +54,7 @@ list(APPEND ATen_CUDA_TEST_SRCS
   ${CMAKE_CURRENT_SOURCE_DIR}/cuda_distributions_test.cu
   ${CMAKE_CURRENT_SOURCE_DIR}/cuda_optional_test.cu
   ${CMAKE_CURRENT_SOURCE_DIR}/cuda_packedtensoraccessor_test.cu
+  ${CMAKE_CURRENT_SOURCE_DIR}/cuda_reportMemoryUsage_test.cpp
   ${CMAKE_CURRENT_SOURCE_DIR}/cuda_vectorized_test.cu
   ${CMAKE_CURRENT_SOURCE_DIR}/cuda_dlconvertor_test.cpp
   ${CMAKE_CURRENT_SOURCE_DIR}/cuda_generator_test.cu

aten/src/ATen/test/reportMemoryUsage.h
@@ -1,0 +1,40 @@
+#pragma once
+
+#include <ATen/ATen.h>
+
+#include <c10/core/Allocator.h>
+#include <c10/util/ThreadLocalDebugInfo.h>
+
+class TestMemoryReportingInfo : public c10::MemoryReportingInfoBase {
+ public:
+  struct Record {
+    void* ptr;
+    int64_t alloc_size;
+    int64_t total_allocated;
+    int64_t total_reserved;
+    c10::Device device;
+  };
+
+  std::vector<Record> records;
+
+  TestMemoryReportingInfo() = default;
+  ~TestMemoryReportingInfo() override = default;
+
+  void reportMemoryUsage(
+      void* ptr,
+      int64_t alloc_size,
+      int64_t total_allocated,
+      int64_t total_reserved,
+      c10::Device device) override {
+    records.emplace_back(
+        Record{ptr, alloc_size, total_allocated, total_reserved, device});
+  }
+
+  bool memoryProfilingEnabled() const override {
+    return true;
+  }
+
+  Record getLatestRecord() {
+    return records.back();
+  }
+};

c10/core/Allocator.cpp
@@ -43,11 +43,17 @@ bool memoryProfilingEnabled() {
   return reporter_ptr && reporter_ptr->memoryProfilingEnabled();
 }
 
-void reportMemoryUsageToProfiler(void* ptr, int64_t alloc_size, Device device) {
+void reportMemoryUsageToProfiler(
+    void* ptr,
+    int64_t alloc_size,
+    int64_t total_allocated,
+    int64_t total_reserved,
+    Device device) {
   auto* reporter_ptr = static_cast<MemoryReportingInfoBase*>(
       ThreadLocalDebugInfo::get(DebugInfoKind::PROFILER_STATE));
   if (reporter_ptr) {
-    reporter_ptr->reportMemoryUsage(ptr, alloc_size, device);
+    reporter_ptr->reportMemoryUsage(
+        ptr, alloc_size, total_allocated, total_reserved, device);
   }
 }
 

c10/core/Allocator.h
@@ -228,10 +228,19 @@ struct C10_API MemoryReportingInfoBase : public c10::DebugInfoBase {
   MemoryReportingInfoBase();
   virtual ~MemoryReportingInfoBase() {}
 
-  // Negative alloc_size corresponds to freeing of the memory
+  /**
+   * alloc_size corresponds to the size of the ptr.
+   *
+   * total_allocated corresponds to total allocated memory.
+   *
+   * total_reserved corresponds to total size of memory pool, both used and
+   * unused, if applicable.
+   */
   virtual void reportMemoryUsage(
       void* ptr,
       int64_t alloc_size,
+      int64_t total_allocated,
+      int64_t total_reserved,
       Device device) = 0;
 
   virtual bool memoryProfilingEnabled() const = 0;
@@ -241,6 +250,8 @@ C10_API bool memoryProfilingEnabled();
 C10_API void reportMemoryUsageToProfiler(
     void* ptr,
     int64_t alloc_size,
+    int64_t total_allocated,
+    int64_t total_reserved,
     Device device);
 
 } // namespace c10

commit 3115029e277cdd667c7c363f5dc7063d5110111a

Fix lint errors in cuda_ReportMemoryUsage tests (#62778)
Summary:
Introduced in https://github.com/pytorch/pytorch/commit/8963ac6105eba7fefc39749902ece5f2ae06e4dc

Pull Request resolved: https://github.com/pytorch/pytorch/pull/62778

Reviewed By: chaekit, driazati

Differential Revision: D30120245

Pulled By: malfet

fbshipit-source-id: 2cb5755b870182dd147a6685c74f7defcc10030a

torch/csrc/autograd/profiler_kineto.cpp
@@ -156,15 +156,15 @@ struct KinetoThreadLocalState : public ProfilerThreadLocalState {
           act.addMetadata("Reserved Bytes", std::to_string(total_reserved));
         }
 #endif // USE_KINETO
-	
-	kineto_events_.emplace_back();
-	auto& evt = kineto_events_.back();
-	      evt.name(kMemoryEventName)
-		.startUs(start_time)
-		.deviceIndex(device.index())
-		.deviceType(device.type())
-		.nBytes(alloc_size)
-		.startThreadId(at::RecordFunction::currentThreadId());
+
+        kineto_events_.emplace_back();
+        auto& evt = kineto_events_.back();
+        evt.name(kMemoryEventName)
+          .startUs(start_time)
+          .deviceIndex(device.index())
+          .deviceType(device.type())
+          .nBytes(alloc_size)
+          .startThreadId(at::RecordFunction::currentThreadId());
     }
   }
 

aten/src/ATen/test/cuda_reportMemoryUsage_test.cpp
@@ -1,4 +1,4 @@
-#include "reportMemoryUsage.h"
+#include <ATen/test/reportMemoryUsage.h>
 
 #include <gtest/gtest.h>
 

aten/src/ATen/test/reportMemoryUsage_test.cpp
@@ -1,4 +1,4 @@
-#include "reportMemoryUsage.h"
+#include <ATen/test/reportMemoryUsage.h>
 
 #include <gtest/gtest.h>
 