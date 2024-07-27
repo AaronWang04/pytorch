AaronWang04/pytorch

commit 622baf0b355eef51782e6df93626d7deff44a69f

[1/N] Add missing constructors or assignment operators (#131077)
Just mark them as deleted in most cases.
Pull Request resolved: https://github.com/pytorch/pytorch/pull/131077
Approved by: https://github.com/ezyang

aten/src/ATen/code_template.h
@@ -19,6 +19,7 @@ namespace at::jit {
 struct TemplateEnv {
   TemplateEnv() = default;
   TemplateEnv(TemplateEnv& parent) : parent(&parent) {}
+  TemplateEnv& operator==(const TemplateEnv& parent) = delete;
 
   using string_list = std::vector<std::string>;
 

c10/core/SymbolicShapeMeta.h
@@ -23,6 +23,8 @@ class C10_API SymbolicShapeMeta {
 
   SymbolicShapeMeta() = default;
   SymbolicShapeMeta(const SymbolicShapeMeta& other);
+  SymbolicShapeMeta& operator==(const SymbolicShapeMeta& other) = delete;
+  SymbolicShapeMeta& operator==(SymbolicShapeMeta&& other) = delete;
 
   void refresh_numel() {
     // Non-const, don't need to hold mutables_ lock

c10/core/TensorImpl.h
@@ -255,6 +255,9 @@ struct C10_API ExtraMeta {
       custom_storage_error_msg_ = other.custom_storage_error_msg_;
     }
   }
+  ExtraMeta& operator==(const ExtraMeta& other) = delete;
+  ExtraMeta(ExtraMeta&& other) = delete;
+  ExtraMeta& operator==(ExtraMeta&& other) = delete;
 
   ExtraMeta(
       std::unique_ptr<c10::SymbolicShapeMeta> symbolic_shape_meta,

torch/csrc/Exceptions.h
@@ -166,6 +166,9 @@ struct python_error : public std::exception {
     other.traceback = nullptr;
   }
 
+  python_error& operator==(const python_error& other) = delete;
+  python_error& operator==(python_error&& other) = delete;
+
   // NOLINTNEXTLINE(bugprone-exception-escape)
   ~python_error() override {
     if (type || value || traceback) {

torch/csrc/cuda/CUDAPluggableAllocator.h
@@ -73,6 +73,7 @@ struct TORCH_CUDA_CPP_API CUDAPluggableAllocator
       std::function<FreeFuncType> free_fn);
 
   CUDAPluggableAllocator(CUDAPluggableAllocator& other);
+  CUDAPluggableAllocator& operator==(CUDAPluggableAllocator& other) = delete;
 
   void set_init_fn(std::function<void(int)> init_fn);
 

torch/csrc/utils/invalid_arguments.cpp
@@ -113,10 +113,9 @@ struct Option {
   Option(bool is_variadic, bool has_out)
       : arguments(), is_variadic(is_variadic), has_out(has_out){};
   Option(const Option&) = delete;
-  Option(Option&& other) noexcept
-      : arguments(std::move(other.arguments)),
-        is_variadic(other.is_variadic),
-        has_out(other.has_out){};
+  Option(Option&& other) noexcept = default;
+  Option& operator==(const Option&) = delete;
+  Option& operator==(Option&&) = delete;
 
   std::vector<Argument> arguments;
   bool is_variadic;

aten/src/ATen/cuda/detail/DeviceThreadHandles.h
@@ -45,7 +45,7 @@ struct DeviceThreadHandlePool : public std::enable_shared_from_this<DeviceThread
     // unordered_map<int, vector<unique_ptr<Handle>>> created_handles;
     Handle(const Handle& rhs) = delete;
     // Following https://stackoverflow.com/questions/3279543/what-is-the-copy-and-swap-idiom
-    Handle(Handle&& rhs) : Handle() { std::swap(handle, rhs.handle); }
+    Handle(Handle&& rhs) noexcept : Handle() { std::swap(handle, rhs.handle); }
     // operator= takes argument by value
     Handle& operator=(Handle rhs) { std::swap(handle, rhs.handle); return *this; }
     ~Handle() {

c10/core/SafePyObject.h
@@ -26,6 +26,8 @@ struct C10_API SafePyObject {
   SafePyObject(SafePyObject&& other) noexcept
       : data_(std::exchange(other.data_, nullptr)),
         pyinterpreter_(other.pyinterpreter_) {}
+  // For now it's not used, so we just disallow it.
+  SafePyObject& operator=(SafePyObject&&) = delete;
 
   // In principle this could be copyable if we add an incref to PyInterpreter
   // but for now it's easier to just disallow it.

commit 03cfb36df916a8eb2c752393c5632c8fbecf740d

Fix typo in assignment operators (#131890)
Most typos were introduced in #131077
Pull Request resolved: https://github.com/pytorch/pytorch/pull/131890
Approved by: https://github.com/Skylion007

aten/src/ATen/code_template.h
@@ -19,7 +19,7 @@ namespace at::jit {
 struct TemplateEnv {
   TemplateEnv() = default;
   TemplateEnv(TemplateEnv& parent) : parent(&parent) {}
-  TemplateEnv& operator==(const TemplateEnv& parent) = delete;
+  TemplateEnv& operator=(const TemplateEnv& parent) = delete;
 
   using string_list = std::vector<std::string>;
 

c10/core/SymbolicShapeMeta.h
@@ -23,8 +23,8 @@ class C10_API SymbolicShapeMeta {
 
   SymbolicShapeMeta() = default;
   SymbolicShapeMeta(const SymbolicShapeMeta& other);
-  SymbolicShapeMeta& operator==(const SymbolicShapeMeta& other) = delete;
-  SymbolicShapeMeta& operator==(SymbolicShapeMeta&& other) = delete;
+  SymbolicShapeMeta& operator=(const SymbolicShapeMeta& other) = delete;
+  SymbolicShapeMeta& operator=(SymbolicShapeMeta&& other) = delete;
 
   void refresh_numel() {
     // Non-const, don't need to hold mutables_ lock

c10/core/TensorImpl.h
@@ -255,9 +255,9 @@ struct C10_API ExtraMeta {
       custom_storage_error_msg_ = other.custom_storage_error_msg_;
     }
   }
-  ExtraMeta& operator==(const ExtraMeta& other) = delete;
+  ExtraMeta& operator=(const ExtraMeta& other) = delete;
   ExtraMeta(ExtraMeta&& other) = delete;
-  ExtraMeta& operator==(ExtraMeta&& other) = delete;
+  ExtraMeta& operator=(ExtraMeta&& other) = delete;
 
   ExtraMeta(
       std::unique_ptr<c10::SymbolicShapeMeta> symbolic_shape_meta,

torch/csrc/Exceptions.h
@@ -166,8 +166,8 @@ struct python_error : public std::exception {
     other.traceback = nullptr;
   }
 
-  python_error& operator==(const python_error& other) = delete;
-  python_error& operator==(python_error&& other) = delete;
+  python_error& operator=(const python_error& other) = delete;
+  python_error& operator=(python_error&& other) = delete;
 
   // NOLINTNEXTLINE(bugprone-exception-escape)
   ~python_error() override {

torch/csrc/cuda/CUDAPluggableAllocator.h
@@ -73,7 +73,7 @@ struct TORCH_CUDA_CPP_API CUDAPluggableAllocator
       std::function<FreeFuncType> free_fn);
 
   CUDAPluggableAllocator(CUDAPluggableAllocator& other);
-  CUDAPluggableAllocator& operator==(CUDAPluggableAllocator& other) = delete;
+  CUDAPluggableAllocator& operator=(CUDAPluggableAllocator& other) = delete;
 
   void set_init_fn(std::function<void(int)> init_fn);
 

torch/csrc/utils/invalid_arguments.cpp
@@ -114,8 +114,8 @@ struct Option {
       : arguments(), is_variadic(is_variadic), has_out(has_out){};
   Option(const Option&) = delete;
   Option(Option&& other) noexcept = default;
-  Option& operator==(const Option&) = delete;
-  Option& operator==(Option&&) = delete;
+  Option& operator=(const Option&) = delete;
+  Option& operator=(Option&&) = delete;
 
   std::vector<Argument> arguments;
   bool is_variadic;